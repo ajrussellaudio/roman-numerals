74e4a4b4b496b8709100626f89f996a0562619fc
Alan Russell <ajrussellaudio@gmail.com>
2018-10-25 12:09:47 +0100

    refactor to assign numerals as class property

--- src/RomanNumeralGenerator.ts
+++ src/RomanNumeralGenerator.ts
@@ -1,14 +1,15 @@
 export class RomanNumeralGenerator {
+  numerals = {
+    1000: "M",
+    500: "D",
+    100: "C",
+    50: "L",
+    10: "X",
+    5: "V",
+    1: "I"
+  };
+
   generate(number: number) {
-    const numerals = {
-      1: "I",
-      5: "V",
-      10: "X",
-      50: "L",
-      100: "C",
-      500: "D",
-      1000: "M"
-    };
-    return numerals[number];
+    return this.numerals[number];
   }
 }
