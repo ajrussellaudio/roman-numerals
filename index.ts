f1a785d8c08bb81dd4ada28f6a2d7f045331def8
Alan Russell <ajrussellaudio@gmail.com>
2018-10-25 18:19:04 +0100

    add message on invalid input

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 import { RomanNumeralGenerator } from "./src/RomanNumeralGenerator";
 
-const number: number = parseInt(process.argv[2]);
-const roman: RomanNumeralGenerator = new RomanNumeralGenerator();
-const numeral: string = roman.generate(number);
+if (!process.argv[2]) {
+  console.error(
+    "Invalid input!\nPlease pass a number as an argument to run, e.g.\n\tnpm start -- 138"
+  );
+} else {
+  const number: number = parseInt(process.argv[2]);
+  const roman: RomanNumeralGenerator = new RomanNumeralGenerator();
+  const numeral: string = roman.generate(number);
 
-console.log(number + " as a Roman numeral is:");
-console.log(numeral);
+  console.log(number + " as a Roman numeral is:");
+  console.log(numeral);
+}
